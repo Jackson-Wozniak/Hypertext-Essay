edb28f2873d06da624ff8943533a696035bf53c8
Jackson Wozniak <jjw30@comcast.net>
2025-04-23 14:28:52 -0400

    when pressing an entry in the map, it opens the popup that will display the corresponding text

---
+++ index.js
@@ -0,0 +1,38 @@
+let pagesRead = 0;
+let totalPages = 10;
+
+let titlesPagesRead = [];
+
+const pageCounterDiv = document.getElementById("page-counter");
+const entryPopupWindowDiv = document.getElementById("entry-popup-window");
+pageCounterDiv.textContent = `${pagesRead} / ${totalPages} read`;
+
+function incrementCounter(){
+    if(pagesRead + 1 >= totalPages){
+        pageCounterDiv.textContent = 'How do computers work?';
+        pageCounterDiv.className = 'counter-complete';
+        return;
+    }
+    pagesRead++;
+    pageCounterDiv.textContent = `${pagesRead} / ${totalPages} read`;
+}
+
+function setPageRead(pageName){
+    if(!titlesPagesRead.includes(pageName)){
+        incrementCounter();
+        titlesPagesRead.push(pageName);
+        let tempEntry = document.getElementById("hello-world-window");
+        tempEntry.classList.remove('entry-unread');
+        tempEntry.classList.add('entry-read');
+    }
+}
+
+function openEntryWindow(pageName){
+    setPageRead(pageName);
+    entryPopupWindowDiv.classList.add("visible");
+}
+
+function closeEntryWindow(){
+    entryPopupWindowDiv.hidden = true;
+    entryPopupWindowDiv.classList.remove("visible");
+}
